ff0cfd1ed19a4cb63dc34f38c865c7f162eef1c8
RaineShadow <twitch.raineshadow@gmail.com>
2025-12-01 14:33:13 -0500

    FIRST PART COMPLETED SUCCESSFULLY!

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const readLine = require('readline');
 
 //Read File Line by Line
 async function processLineByLine(){
-	const fileStream = fs.createReadStream('input_password_test.txt');
+	const fileStream = fs.createReadStream('input_password.txt');
 
 	const rl = readLine.createInterface({
 		input: fileStream,
@@ -36,6 +36,10 @@ function moveDial(line){
 	}
 
 	console.log("dial currently at: " + current_password_dial);
+
+	if(current_password_dial == 0){
+		dial_at_zero++;
+	}
 }
 
 //-------------------------------Start of Code
@@ -44,6 +48,9 @@ console.log("Advent of Code 2025");
 
 //dial goes from 0 to 99
 let password_dial = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99];
-let current_password_dial = 11;
+let current_password_dial = 50;
+let dial_at_zero = 0;
 
-processLineByLine();
+processLineByLine().then(temp => {
+	console.log("Password is: " + dial_at_zero);
+});
