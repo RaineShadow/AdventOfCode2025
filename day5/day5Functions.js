05bc36a526d69b561d5273e25104bb815349c4b8
RaineShadow <twitch.raineshadow@gmail.com>
2025-12-05 09:52:12 -0500

    Day 5 part 1 complete.

--- day5/day5Functions.js
+++ day5/day5Functions.js
@@ -6,9 +6,14 @@ export function day5(){
 	processLineByLine();
 }
 
+let doneWithRanges = false;
+let ingredientRange = [];
+let count = 0;
+let freshIngredients = 0;
+
 //Read File Line by Line
 async function processLineByLine(){
-	const fileStream = fs.createReadStream('./day5/input_ingredient_test.txt');
+	const fileStream = fs.createReadStream('./day5/input_ingredient.txt');
 
 	const rl = readLine.createInterface({
 		input: fileStream,
@@ -18,5 +23,68 @@ async function processLineByLine(){
 	for await(const line of rl){
 		console.log("");
 		console.log("line: " + line);
+
+		if(!doneWithRanges){
+			processRanges(line);
+		} else{
+			let isItFresh = isFresh(line);
+
+			if(isItFresh){
+				freshIngredients++;
+			}
+		}
+
+		//console.log("ingredientRange: " + ingredientRange);
 	}
+
+	console.log("Fresh ingredients: " + freshIngredients);
+}
+
+function processRanges(line){
+	if(line == ''){
+		console.log("break");
+		doneWithRanges = true;
+	} else {
+		ingredientRange[count] = line;
+		count++;
+	}
+}
+
+function isFresh(line){
+	//For each range
+	for(let i = 0; i < ingredientRange.length; i++){
+		let firstNumberString = '';
+		let lastNumberString = '';
+		let startLastNumber = false;
+
+		//Get the numbers of that range
+		for(let j = 0; j < ingredientRange[i].length; j++){
+			if(ingredientRange[i].charAt(j) == '-'){
+				startLastNumber = true;
+				j++;
+			}
+
+			if(!startLastNumber){
+				firstNumberString += ingredientRange[i].charAt(j);
+			} else{
+				lastNumberString += ingredientRange[i].charAt(j);
+			}
+		}
+
+		//console.log("firstNumber: " + firstNumberString + " lastNumber: " + lastNumberString);
+
+		let lineNumber = parseInt(line);
+		let firstNumber = parseInt(firstNumberString);
+		let lastNumber = parseInt(lastNumberString);
+
+		if(lineNumber >= firstNumber && lineNumber <= lastNumber){
+			return true;
+		}
+
+		if(i == (ingredientRange.length - 1)){
+			return false;
+		}
+	}
+
+	return false;
 }
